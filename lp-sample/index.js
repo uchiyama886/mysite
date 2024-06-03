1fef94ed3a69141daf51069e98a532bbecc157d9
uciyama886 <xneishan51@gmail.com>
2024-06-03 13:49:55 +0900

    初期化
    あああ

--- lp-sample/index.js
+++ lp-sample/index.js
@@ -1,3 +1,14 @@
-function handleClick() {
-  alert('Hello, world!');
+const typingAnimation = document.querySelector('.typing-animation');
+const textToType = "Hello World!"; // タイプする文字列
+let charIndex = 0;
+
+function typeLetter() {
+  const char = textToType[charIndex];
+  typingAnimation.textContent += char;
+  charIndex++;
+  if (charIndex === textToType.length) {
+    clearInterval(intervalId);
+  }
 }
+
+const intervalId = setInterval(typeLetter, 100);
